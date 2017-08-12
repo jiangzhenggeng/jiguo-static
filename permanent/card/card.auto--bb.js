324990afd55e1842200c0ff0f6b0d40b24ff89a3
JianZG <2992265870@qq.com>
2017-08-12 17:43:16 +0800

    $DATE

--- permanent/card/card.auto--bb.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    var iframe = $('iframe');
-    for(var i = 0; i<iframe.length ; i++ ){
-        if(typeof iframe.get(i).getAttribute('data-productid')!='undefined'){
-            iframe.eq(i).css('height', iframe.eq(i).width()/2.993325 + 38 );
-        }
-    }
-});
--- zdm/admin/develope/script/app/event/event_list.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by jiangzg on 16/7/15.
- */
