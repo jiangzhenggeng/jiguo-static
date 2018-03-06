28049b4841d9579ac6daf81c69e800d7d6fba91f
jmc <935453286@qq.com>
2018-03-06 15:58:09 +0800

    编辑器复用

--- zdm/static/develope/js/build_config.js
+++ zdm/static/develope/js/build_config.js
@@ -1,6 +1,6 @@
 requirejs.config({
 
-    baseUrl: 'http://cdn.jiguo.com/zdm/static/version_1.5/js/',
+    baseUrl: 'http://cdn.jiguo.com/zdm/static/version_1.6/js/',
 
     waitSeconds: 0,
 
@@ -9,24 +9,15 @@ requirejs.config({
         'jquery': 'lib/jquery.min',
         'layer': 'lib/layer/layer',
         'laydate':'lib/laydate/laydate',
-			  'cropper':'lib/cropper/cropper',
+        'cropper':'lib/cropper/cropper',
         'template':'lib/template-native',
-        'ueconfig':'http://zdm.jiguo.com/protected/extensions/editor/ueditor.config',
-        'ueditor':'http://zdm.jiguo.com/protected/extensions/editor/ueditor',
-        'zeroclipboard':'http://zdm.jiguo.com/protected/extensions/editor/third-party/zeroclipboard/ZeroClipboard.min',
     },
     shim: {
         'layer':{
             deps: ['css!lib/layer/need/layer.css'],
         },
-			'cropper':{
-				deps:['css!lib/cropper/cropper']
-			},
-        'ueditor': {
-            deps: [
-                'http://www.jiguo.com/protected/extensions/ueditor/third-party/zeroclipboard/ZeroClipboard.min.js',
-                'http://www.jiguo.com/protected/extensions/ueditor/ueditor.config.js'
-            ],
+        'cropper':{
+            deps:['css!lib/cropper/cropper']
         }
     }
 });
--- zdm/static/tools/build.js
+++ zdm/static/tools/build.js
@@ -9,16 +9,13 @@
     //findNestedDependencies设置为true表示将所有相关的依赖模块也合并进来，
     // 默认为false只会对main.js进行压缩合并的操作
     findNestedDependencies: false,
-    dir: "../version_1.5",//输出目录
+    dir: "../version_1.6",//输出目录
     optimizeCss:'standard',
     fileExclusionRegExp:'\.(less|html|php|psd|PSD$|DS_Store)$',//匹配的文件不会输出
     paths: {
         'jquery': 'lib/jquery.min',
         'layer': 'lib/layer/layer',
         'laydate': 'lib/laydate/laydate',
-        'template':'lib/template-native',
-        'ueconfig':'http://zdm.jiguo.com/protected/extensions/editor/ueditor.config',
-        'ueditor':'http://zdm.jiguo.com/protected/extensions/editor/ueditor',
-        'zeroclipboard':'http://zdm.jiguo.com/protected/extensions/editor/third-party/zeroclipboard/ZeroClipboard.min'
+        'template':'lib/template-native'
     }
 })
